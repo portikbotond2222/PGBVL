35abd4c713113c82881e29250d2a131c79d8eaed
Vass Lehel <methode222@gmail.com>
2018-12-20 17:14:04 +0200

    test commit

--- AndroidProject/MyApplication5/app/src/main/java/com/example/portik/myapplication5/MainActivity.java
+++ AndroidProject/MyApplication5/app/src/main/java/com/example/portik/myapplication5/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.portik.myapplication5;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginRegBtn;
     private Button LogoutBtn;
     private FirebaseAuth myAuth;
+    public static final int CAMERA_REQUEST=9999;
+    private ImageView imageView;
+    private Button cameraBtn;
+
+    private String photoPath;
 
 
     @Override
@@ -29,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         loginRegBtn = (Button) findViewById(R.id.loginreg_buton);
         LogoutBtn =  (Button) findViewById(R.id.LogoutBtn);
 
+        cameraBtn=(Button) findViewById(R.id.camera);
+
+
         LogoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,6 +59,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 redirectToLoginReg();
+            }
+        });
+
+        imageView=(ImageView) findViewById(R.id.imageView);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera(view);
             }
         });
     }
@@ -77,4 +105,51 @@ public class MainActivity extends AppCompatActivity {
         startActivity(mainIntent);
         finish();
     }
+
+    public void openCamera(View view)
+    {
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(photoFile != null)
+            {
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.portik.myapplication5",photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+                startActivityForResult(intent,CAMERA_REQUEST);
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CAMERA_REQUEST)
+        {
+            Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private File createImageFile() throws IOException{
+        String tmp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imgFileName="JPEG_"+tmp + " _";
+        File storageDir=getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image=File.createTempFile(
+                imgFileName,
+                ".jpg",
+                storageDir
+        );
+
+        photoPath=image.getAbsolutePath();
+        return image;
+    }
+
 }
