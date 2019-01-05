bc046eb8dc8d565b17d9b34fe21a75a433a889c9
Portik Botond <portik.botond@innoweb.ro>
2019-01-05 17:16:17 +0200

    test

--- MyApplication5/app/src/main/java/com/example/portik/myapplication5/MainActivity.java
+++ MyApplication5/app/src/main/java/com/example/portik/myapplication5/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button LogoutBtn;
     private FirebaseAuth myAuth;
 
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
