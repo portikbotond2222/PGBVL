837889621adafe32e9d57fb055b84ed89c1c14c1
Portik Botond <portik.botond@innoweb.ro>
2019-01-09 16:43:49 +0200

    add data to firebasestore

--- MyApplication5/app/src/main/java/com/example/portik/myapplication5/MyProfile.java
+++ MyApplication5/app/src/main/java/com/example/portik/myapplication5/MyProfile.java
@@ -1,31 +1,28 @@
 package com.example.portik.myapplication5;
 
-import android.nfc.Tag;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class MyProfile extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
+    private FirebaseFirestore db;
+
     private static final String TAG = "MyAuth";
 
     private Button SaveProfile;
@@ -33,6 +30,10 @@ public class MyProfile extends AppCompatActivity {
     private EditText lastName;
     private EditText Address;
     private FirebaseAuth myAuth;
+    CollectionReference dbUserdata;
+    CollectionReference updateData;
+    Userdata ud;
+    String userId;
 
 
     @Override
@@ -41,7 +42,8 @@ public class MyProfile extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
         myAuth = FirebaseAuth.getInstance();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference("userdata");
+        db = FirebaseFirestore.getInstance();
+
         firstName = (EditText) findViewById(R.id.firstnametext);
         lastName = (EditText) findViewById(R.id.lastnametext);
         Address = (EditText) findViewById(R.id.postaltext);
@@ -49,34 +51,66 @@ public class MyProfile extends AppCompatActivity {
         SaveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-               writeNewUser("1",firstName.getText().toString(),lastName.getText().toString(),Address.getText().toString());
+                 userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                String fname = firstName.getText().toString().trim();
+                String lname = lastName.getText().toString().trim();
+                String address = Address.getText().toString().trim();
+                if(validateInputs(userId,fname,lname,address)){
+                     dbUserdata = db.collection("userdata");
+
+                     ud = new Userdata(userId,fname,lname,address);
+                    saveUserdata(dbUserdata,ud);
+//                    updateData = dbUserdata.whereEqualTo("userid", userId).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                            dbUserdata.whereEqualTo("userid", userId).;
+//                        }
+//                    }).addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//
+//                        }
+//                    });
+
+
+                }
             }
         });
     }
 
-    public static class User {
-//        public String userid;
+    public static class Userdata {
+        public String userid;
         public String firstname;
         public String lastname;
         public String adress;
 
 
-        public User(String firstname,String lastname,String adress) {
+        public Userdata(String userid,String firstname,String lastname,String adress) {
             this.firstname = firstname;
             this.lastname = lastname;
-//            this.userid = userid;
+            this.userid = userid;
             this.adress = adress;
         }
     }
 
-
-    private void writeNewUser(String userid, String firstname, String lastname,String adress) {
-        User user = new User(firstname, lastname,adress);
-
-        mDatabase.setValue(user);
+    private boolean validateInputs(String userId, String firstname, String lastname, String Address) {
+        if (userId.isEmpty() || firstname.isEmpty() || lastname.isEmpty() ||Address.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
     }
-
-
-
+    public void saveUserdata(CollectionReference dbUserdata, Userdata ud){
+        dbUserdata.add(ud).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(MyProfile.this,"Userdata added",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MyProfile.this,e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
